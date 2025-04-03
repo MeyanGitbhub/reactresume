43e433b7703fe6a2b39e80b8099a77561bb4f016
BJ <wright.brentonj@gmaill.com>
2025-04-03 12:21:56 -0400

    [React-resume]: Job key

--- resume/app/resume-page/resume-page.tsx
+++ resume/app/resume-page/resume-page.tsx
@@ -3,7 +3,7 @@ import { ResumeIntro } from "~/resume-intro/resume-intro";
 import { useState } from "react";
 
 export function ResumePage() {
-  const [name, setName] = useState('B.J. Wright')
+  const name = 'B.J. Wright';
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -16,7 +16,7 @@ export function ResumePage() {
         <div className="w-full space-y-6 px-4 max-w-7xl">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4 ">
             {jobs.map((job) => (
-              <Job job={job}></Job>
+              <Job job={job} key={job.id} />
             ))}
           </nav>
         </div>
