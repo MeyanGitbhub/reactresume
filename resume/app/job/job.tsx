2012e9dd28c95f8d54929ed4b32dfe1e60ba8ff5
BJ <wright.brentonj@gmaill.com>
2025-04-03 12:06:20 -0400

    [React-resume]: Job description html format fix

--- resume/app/job/job.tsx
+++ resume/app/job/job.tsx
@@ -1,30 +1,23 @@
-export function Job({ job }: { job: JobItem}) {
+export function Job({ job }: { job: JobItem }) {
   return (
     <div className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        {/* {jobs.map(({ id, jobTitle, companyName, jobDescription}) => ( */}
-          <div className="max-w-[700px] w-full space-y-6 px-4" key={job.jobTitle + "-" + job.id}>
-            <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-              <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-                {job.jobTitle} | {job.companyName}
-              </p>
-              <div> </div>
-              <ul>
-                <div>
-                  <div>
-                    {job.jobDescription.map((jobDescription, i) =>
-                      <li className="mb-2" key={job.jobTitle + '-' +  i}>
-                        {jobDescription}
-                      </li>
-                    )}
-                  </div>
-                </div>
 
-              </ul>
-            </nav>
-          </div>
-        {/* ))} */}
-
+        <div className="max-w-[700px] w-full space-y-6 px-4" key={job.jobTitle + "-" + job.id}>
+          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+              {job.jobTitle} | {job.companyName}
+            </p>
+            <div> </div>
+            <ul>
+              {job.jobDescription.map((description, i) => (
+                <li className="mb-2" key={`desc-${job.id}-${i}`}>
+                  {description}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
@@ -33,7 +26,7 @@ export function Job({ job }: { job: JobItem}) {
 
 
 export interface JobItem {
-  id: number; 
+  id: number;
   jobTitle: string;
   companyName: string;
   jobDescription: string[];
